f5b001251e897b08d1ce1888300ae5095955bf73
Franklin Delehelle <franklin.delehelle@etu.enseeiht.fr>
2014-01-29 17:23:42 +0100

    Test

--- Spock1/TestMain.cs
+++ Spock1/TestMain.cs
@@ -8,8 +8,8 @@ namespace Spock
         public static void Main()
         {
             Node n;
-            (new Thread(TestPublisher.test)).Start();
-            //(new Thread((new TestSubscriber()).test)).Start();
+            //(new Thread(TestPublisher.test)).Start();
+            (new Thread((new TestSubscriber()).test)).Start();
         }
     }
 }
